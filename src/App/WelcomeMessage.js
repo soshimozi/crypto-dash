e494fd72523b86125b4af8de6c12675f75576db4
Scott McCain <scott.mccain@gmail.com>
2018-12-16 01:36:31 -0800

    Settings page, local storage

--- src/App/WelcomeMessage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class WelcomeMessage extends React.Component {
-    render() {
-        return <h1>Welcome To Cryptodash!</h1>
-    }
-}
--- src/App/index.js
+++ src/App/index.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import WelcomeMessage from './WelcomeMessage';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
-import {AppProvider} from './AppProvider';
+import AppProvider from './AppProvider';
 
+import Settings from '../Settings';
 class App extends Component {
   render() {
     return (
       <AppLayout>
         <AppProvider>
           <AppBar />
-          <WelcomeMessage />
+          <Settings />
         </AppProvider>
       </AppLayout>
     );
